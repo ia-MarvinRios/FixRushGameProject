a4b50e18fd5358d270b7dea7d8ac38fa8ad1e449
Marvin Raydel Rios Aguilar <ia.marvinrios@ufg.edu.sv>
2026-05-30 22:08:20 -0600

    Update WaterHose.cs

--- Assets/Scripts/WaterHose.cs
+++ Assets/Scripts/WaterHose.cs
@@ -88,7 +88,7 @@ public class WaterHose : Interactable
 
     private void GetReleaseBucket(bool get, PlayerController player)
     {
-        if (get)
+        if (get && player.GrabbedObj != null)
         {
             player.GrabbedObj.transform.parent = _bucketRoot;
             player.GrabbedObj.transform.position = _bucketRoot.position;
